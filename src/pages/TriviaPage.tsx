48f2cf5679660a3828cc2265c10f954f3ec361e4
Juanma <juancrossetto@gmail.com>
2021-11-18 10:10:13 -0300

    trivia add points

--- src/pages/TriviaPage.tsx
+++ src/pages/TriviaPage.tsx
@@ -1,11 +1,13 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useContext, useState } from 'react';
 import { AspectRatio } from '@chakra-ui/react';
 import Layout from '../components/Layout';
 import Spinner from '../components/Spinner';
+import { AuthContext } from '../context/Auth';
 
 interface TriviaPageProps {}
 
 const TriviaPage: FC<TriviaPageProps> = () => {
+  const { user } = useContext(AuthContext);
   const [isLoading, setIsLoading] = useState(true);
 
   return (
@@ -13,7 +15,8 @@ const TriviaPage: FC<TriviaPageProps> = () => {
       <AspectRatio ratio={16 / 8} minH={'450px'}>
         <iframe
           title="trivia-frame"
-          src="https://trivia-semi2.herokuapp.com/"
+          src={`https://trivia-semi2.herokuapp.com/?userId=${user?._id}`}
+          // src={`http://localhost:3000/?userId=${user?._id}`}
           allow="geolocation; microphone; camera; midi; encrypted-media;"
           onLoad={() => setIsLoading(false)}
         />
